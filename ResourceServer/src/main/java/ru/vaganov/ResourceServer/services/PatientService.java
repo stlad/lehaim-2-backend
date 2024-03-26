efa696afbc46dbe5510299501ab1c252ca43bc0d
stlad <82213506+stlad@users.noreply.github.com>
2024-03-26 23:12:17 +0500

    removed template engine

--- ResourceServer/src/main/java/ru/vaganov/ResourceServer/services/PatientService.java
+++ ResourceServer/src/main/java/ru/vaganov/ResourceServer/services/PatientService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.vaganov.ResourceServer.config.DataLoadingConfig;
 import ru.vaganov.ResourceServer.models.Patient;
 import ru.vaganov.ResourceServer.repositories.PatientRepo;
 
