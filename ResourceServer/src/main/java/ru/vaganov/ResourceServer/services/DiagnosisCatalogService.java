278fe689b57bfbd1c2a2883c9dc1ec16540dddb0
Ваганов Владислав <v.vaganov@itq-group.com>
2024-06-19 02:06:37 +0500

    "healthy" diagnosis first in list of diagnoses

--- ResourceServer/src/main/java/ru/vaganov/ResourceServer/services/DiagnosisCatalogService.java
+++ ResourceServer/src/main/java/ru/vaganov/ResourceServer/services/DiagnosisCatalogService.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.vaganov.ResourceServer.exceptions.DiagnosisNotFoundException;
 import ru.vaganov.ResourceServer.mappers.DiagnosisMapper;
+import ru.vaganov.ResourceServer.models.Diagnosis;
 import ru.vaganov.ResourceServer.models.dto.DiagnosisDTO;
 import ru.vaganov.ResourceServer.repositories.DiagnosisRepo;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service @Slf4j
 public class DiagnosisCatalogService {
@@ -20,7 +23,9 @@ public class DiagnosisCatalogService {
     private DiagnosisMapper diagnosisMapper;
 
     public List<DiagnosisDTO> findAll(){
-        return diagnosisMapper.toDto(diagnosisRepo.findAll());
+        LinkedList<Diagnosis> diagnosisList = diagnosisRepo.findAll();
+        diagnosisList.addFirst(diagnosisList.removeLast());
+        return diagnosisMapper.toDto(diagnosisList.stream().toList());
     }
 
 
