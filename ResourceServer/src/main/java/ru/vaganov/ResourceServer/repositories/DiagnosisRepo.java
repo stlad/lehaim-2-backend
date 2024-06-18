3c04c7468162d629651232a792dc498a94162178
Vaganov Vladislav <82213506+stlad@users.noreply.github.com>
2024-06-19 02:08:19 +0500

    Исправление порядка выдачи диагнозов из справочника (#14)

--- ResourceServer/src/main/java/ru/vaganov/ResourceServer/repositories/DiagnosisRepo.java
+++ ResourceServer/src/main/java/ru/vaganov/ResourceServer/repositories/DiagnosisRepo.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.vaganov.ResourceServer.models.Diagnosis;
 
+import java.util.LinkedList;
+
 @Repository
 public interface DiagnosisRepo extends JpaRepository<Diagnosis, Integer> {
 
-
+    LinkedList<Diagnosis> findAll();
 }
