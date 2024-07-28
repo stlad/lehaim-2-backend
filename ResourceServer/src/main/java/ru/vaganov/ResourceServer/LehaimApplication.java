f977a8fbe284fd6cc4f2e10fcdf6df41b6516234
Ваганов Владислав <vaganovlad@gmail.com>
2024-07-28 17:29:30 +0500

    upd

--- ResourceServer/src/main/java/ru/vaganov/ResourceServer/LehaimApplication.java
+++ ResourceServer/src/main/java/ru/vaganov/ResourceServer/LehaimApplication.java
@@ -1,14 +1,12 @@
 package ru.vaganov.ResourceServer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.vaganov.ResourceServer.services.CatalogService;
 
 @SpringBootApplication
-public class ResourceServerApplication {
+public class LehaimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResourceServerApplication.class, args);
+		SpringApplication.run(LehaimApplication.class, args);
 	}
 }
