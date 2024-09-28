1bfb0f3461c59809ef2c0c706cf0ff4c8e714d22
Vaganov Vladislav <vaganovlad@gmail.com>
2024-09-28 19:03:42 +0500

    уточнение просчета, возможно ли построить график

--- lehaim/src/main/java/ru/vaganov/lehaim/services/ChartAnalyzer.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/services/ChartAnalyzer.java
@@ -2,64 +2,70 @@ package ru.vaganov.lehaim.services;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.vaganov.lehaim.dictionary.ChartType;
-import ru.vaganov.lehaim.dictionary.recommendation.RegenerationParameterRanges;
+import ru.vaganov.lehaim.dictionary.MostUsedParameters;
 import ru.vaganov.lehaim.models.Parameter;
 import ru.vaganov.lehaim.models.ParameterResult;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static ru.vaganov.lehaim.dictionary.MostUsedParameters.*;
 
 @Service
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChartAnalyzer {
-    private static final Long NEU = 4L;
-    private static final Long LYMF = 2L;
-    private static final Long MON = 3L;
-    private static final Long PLT = 8L;
-    private static final Long CD19 = 18L;
-    private static final Long CD8 =20L;
-    private static final Long CD4 =19L;
-    private static final Long CD3 =17L;
-    private static final Long IFFy_STIM =36L;
-    private static final Long IFNy_SPON =37L;
-    private static final Long TNFa_STIM =38L;
-    private static final Long TNFa_SPON =39L;
-    private static final Long IL2_STIM =40L;
-    private static final Long IL2_SPON =41L;
-
     /**
      * Проверка возможности нарисовать график на основе текущих указаных параметров обследования
+     *
      * @param results ПАраметры обследования
      * @return Список графиков, которые возможно отообразить
      */
-    public static List<ChartType> getPossibleChartsByTEstId(List<ParameterResult> results){
+    public static List<ChartType> getPossibleChartsByTEstId(List<ParameterResult> results) {
         Set<Long> ids = results.stream().map(ParameterResult::getParameter)
                 .map(Parameter::getId).collect(Collectors.toSet());
         List<ChartType> possibleCharts = new ArrayList<>();
-        for(ChartType chart: ChartType.values()){
-            boolean canBeProcessed = switch (chart){
-                case T_CELL -> checkRes(ids, NEU, LYMF, CD3, CD4, CD8);
-                case B_CELL -> checkRes(ids, NEU, CD19, LYMF, CD8, CD4);
-                case CYTOKINE_PAIRS -> checkRes(ids,TNFa_STIM, TNFa_SPON, IFNy_SPON, IFFy_STIM, IL2_SPON, IL2_STIM );
-                case REGENERATION -> checkRes(ids, NEU, MON, LYMF);
-                case SYSTEMIC_INFLAMMATION -> checkRes(ids, NEU, LYMF, MON, PLT);
+        for (ChartType chart : ChartType.values()) {
+            boolean canBeProcessed = switch (chart) {
+                case T_CELL -> checkContains(ids, NEU, LYMF, CD3, CD4, CD8)
+                        && checkNotZero(results, LYMF, CD4, CD8, CD3);
+                case B_CELL -> checkContains(ids, NEU, CD19, LYMF, CD8, CD4)
+                        && checkNotZero(results, LYMF, CD4, CD8, CD19);
+                case CYTOKINE_PAIRS ->
+                        checkContains(ids, TNFa_STIM, TNFa_SPON, IFNy_SPON, IFFy_STIM, IL2_SPON, IL2_STIM)
+                                && checkNotZero(results, TNFa_SPON, IFNy_SPON, IL2_SPON);
+                case REGENERATION -> checkContains(ids, NEU, MON, LYMF)
+                        && checkNotZero(results, MON, LYMF);
+                case SYSTEMIC_INFLAMMATION -> checkContains(ids, NEU, LYMF, MON, PLT)
+                        && checkNotZero(results, LYMF, MON);
             };
-            if(canBeProcessed){
+            if (canBeProcessed) {
                 possibleCharts.add(chart);
             }
         }
         return possibleCharts;
     }
 
-    protected static Boolean checkRes(Set<Long> actualIds, Long... expectedIds) {
-        return actualIds.containsAll(Arrays.stream(expectedIds).collect(Collectors.toSet()));
+    private static Boolean checkContains(Set<Long> actualIds, MostUsedParameters... params) {
+        return actualIds.containsAll(
+                Arrays.stream(params).map(MostUsedParameters::getId)
+                        .filter(val -> val > 0L)
+                        .collect(Collectors.toSet()));
+    }
+
+    private static Boolean checkNotZero(List<ParameterResult> results, MostUsedParameters... params) {
+        return Arrays.stream(params).allMatch(param -> getValue(results, param.getId()) != 0.);
+    }
+
+    private static Double getValue(List<ParameterResult> results, Long id) {
+        Optional<ParameterResult> resOpt = results.stream()
+                .filter(res -> res.getParameter().getId().equals(id)).findAny();
+        if (resOpt.isPresent()) {
+            return resOpt.get().getValue();
+        }
+        return 0.;
     }
 }
