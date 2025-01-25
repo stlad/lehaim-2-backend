61f294322c2686235ee095c25531f257019fc43c
Vaganov Vladislav <vaganovlad@gmail.com>
2025-01-25 13:27:11 +0500

    обновление диапазонов цитокиновых пар

--- lehaim/src/main/java/ru/vaganov/lehaim/dictionary/recommendation/CytokineParameterRange.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/dictionary/recommendation/CytokineParameterRange.java
@@ -6,11 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CytokineParameterRange implements ParameterRange{
-    RANGE_1(0d, 40d),
-    RANGE_2(40d, 80d),
-    RANGE_3(80d, 120d),
-    RANGE_4(120d, 160d),
-    RANGE_5(160d, 10000d);
+    RANGE_1(0d, 26.68),
+    RANGE_2(26.68, 40d),
+    RANGE_3(40d, 80d),
+    RANGE_4(80d, 120d),
+    RANGE_5(120d, 160d),
+    RANGE_6(160d, 10000d);
 
     private final Double startRange;
     private final Double endRange;
