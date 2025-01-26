840645eb4f276985fc52e0abecdaed54c300c5c6
Vaganov Vladislav <82213506+stlad@users.noreply.github.com>
2025-01-26 15:19:12 +0500

    Обновление диапазонов (#52)
    * обновление диапазонов регенерации
    
    * обновление диапазонов системного воспаления
    
    * обновление диапазонов цитокиновых пар
    
    * удаление старых рекомендаций

--- lehaim/src/main/java/ru/vaganov/lehaim/dictionary/recommendation/SIParameterRanges.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/dictionary/recommendation/SIParameterRanges.java
@@ -8,9 +8,10 @@ public class SIParameterRanges {
     @AllArgsConstructor
     public enum Siri implements ParameterRange {
         RANGE_1(0d, 0.25),
-        RANGE_2(0.25, 1.34),
-        RANGE_3(1.34, 4.),
-        RANGE_4(4., 10000.);
+        RANGE_2(0.25, 0.61),
+        RANGE_3(0.61, 1.34),
+        RANGE_4(1.34, 4.),
+        RANGE_5(4., 10000.);
 
         private final Double startRange;
         private final Double endRange;
@@ -26,10 +27,13 @@ public class SIParameterRanges {
     @Getter
     @AllArgsConstructor
     public enum NeuDensity implements ParameterRange {
-        RANGE_1(0d, 1.43),
-        RANGE_2(1.43, 1.51),
-        RANGE_3(1.51, 4.51),
-        RANGE_4(4.51, 10000.);
+        RANGE_1(0d, 0.51),
+        RANGE_2(0.51, 0.72),
+        RANGE_3(0.72, 0.96),
+        RANGE_4(0.96, 1.43),
+        RANGE_5(1.43, 1.51),
+        RANGE_6(1.51, 4.51),
+        RANGE_7(4.51, 10000.);
 
         private final Double startRange;
         private final Double endRange;
@@ -46,9 +50,11 @@ public class SIParameterRanges {
     @AllArgsConstructor
     public enum Piv implements ParameterRange {
         RANGE_1(0d, 37.8),
-        RANGE_2(37.8, 466.68),
-        RANGE_3(466.68, 1400.02),
-        RANGE_4(1400.02, 10000.);
+        RANGE_2(37.8, 180.61),
+        RANGE_3(180.61, 466.68),
+        RANGE_4(466.68, 1400.02),
+        RANGE_5(1400.02, 10000.01),
+        RANGE_6(10000.01, 25000.);
 
         private final Double startRange;
         private final Double endRange;
