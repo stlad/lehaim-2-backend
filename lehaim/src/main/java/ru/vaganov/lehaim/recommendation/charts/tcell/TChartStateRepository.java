d4cea7ac5c86c6ead4f39cfb96a7eadecffb7dd6
Vaganov Vladislav <vaganovlad@gmail.com>
2025-02-17 22:49:35 +0500

    обновление Т-клеток

--- lehaim/src/main/java/ru/vaganov/lehaim/recommendation/charts/tcell/TChartStateRepository.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/recommendation/charts/tcell/TChartStateRepository.java
@@ -12,12 +12,11 @@ public interface TChartStateRepository extends JpaRepository<TChartState, UUID> 
 
     @Query("SELECT c FROM TChartState c " +
             "WHERE c.diagnosis.id = :#{#diagnosis.id} " +
-            "AND c.cd4compareCd8 = :#{#cd4compareCd8} " +
-            "AND c.rangeCd4 = :#{#rangeCd4} " +
-            "AND c.rangeNeuLymf = :#{#rangeNeuLymf}")
-    Optional<TChartState> findState(@Param("diagnosis") Diagnosis diagnosis,
-                                           @Param("cd4compareCd8")  Integer cd4compareCd8,
-                                           @Param("rangeCd4")  TParameterRanges.CD4 rangeCd4,
-                                           @Param("rangeNeuLymf") TParameterRanges.NEU_LYMF rangeNeuLymf);
-
+            "AND c.cd4compareCd8 = :#{#state.cd4compareCd8} " +
+            "AND c.rangeCd4 = :#{#state.rangeCd4} " +
+            "AND c.rangeNeuLymf = :#{#state.rangeNeuLymf} " +
+            "AND c.rangeNeuCd3 = :#{#state.rangeNeuCd3} " +
+            "AND c.rangeNeuCd4 = :#{#state.rangeNeuCd4} " +
+            "AND c.rangeNeuCd8 = :#{#state.rangeNeuCd8} ")
+    Optional<TChartState> findState(@Param("diagnosis") Diagnosis diagnosis, @Param("state") TChartState state);
 }
--- lehaim/src/main/java/ru/vaganov/lehaim/recommendation/charts/tcell/TParameterRanges.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/recommendation/charts/tcell/TParameterRanges.java
@@ -10,6 +10,61 @@ import java.util.Arrays;
 public class TParameterRanges {
     @Getter
     @AllArgsConstructor
+    public enum NEU_CD3 implements ParameterRange {
+
+        RANGE_1(0., 1.31),
+        RANGE_2(1.32, 1.84),
+        RANGE_3(1.84, 2.63),
+        RANGE_4(2.63, 3.65),
+        RANGE_5(3.65, 10.93),
+        RANGE_6(10.93, 50.);
+
+        private final Double startRange;
+        private final Double endRange;
+
+        public static TParameterRanges.NEU_CD3 of(Double value) {
+            return ParameterRangeUtils.of(value, TParameterRanges.NEU_CD3.values());
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum NEU_CD4 implements ParameterRange {
+        RANGE_1(0., 0.75),
+        RANGE_2(1.75, 2.45),
+        RANGE_3(2.45, 3.5),
+        RANGE_4(3.5, 5.1),
+        RANGE_5(5.1, 15.01),
+        RANGE_6(15.01, 100.);
+
+        private final Double startRange;
+        private final Double endRange;
+
+        public static TParameterRanges.NEU_CD4 of(Double value) {
+            return ParameterRangeUtils.of(value, TParameterRanges.NEU_CD4.values());
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum NEU_CD8 implements ParameterRange {
+        RANGE_1(0., 5.53),
+        RANGE_2(5.53, 7.74),
+        RANGE_3(7.74, 11.05),
+        RANGE_4(11.05, 12.32),
+        RANGE_5(12.32, 36.93),
+        RANGE_6(36.93, 100.);
+
+        private final Double startRange;
+        private final Double endRange;
+
+        public static TParameterRanges.NEU_CD8 of(Double value) {
+            return ParameterRangeUtils.of(value, TParameterRanges.NEU_CD8.values());
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
     public enum NEU_LYMF implements ParameterRange {
         RANGE_1(0d, 0.5),
         RANGE_2(0.5, 0.85),
@@ -40,7 +95,7 @@ public class TParameterRanges {
 
         public static TParameterRanges.CD4 of(Double value) {
             return ParameterRangeUtils.of(value,
-                    Arrays.stream(CD4.values()).filter(e->e.getStartRange()!=null).toArray(TParameterRanges.CD4[]::new));
+                    Arrays.stream(CD4.values()).filter(e -> e.getStartRange() != null).toArray(TParameterRanges.CD4[]::new));
         }
     }
 }
