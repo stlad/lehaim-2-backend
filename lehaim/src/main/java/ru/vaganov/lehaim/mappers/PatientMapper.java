b9d07d84ed3e735b932fe433cede62e72c378f32
Vaganov Vladislav <vaganovlad@gmail.com>
2025-03-08 12:13:59 +0500

    тесты

--- lehaim/src/main/java/ru/vaganov/lehaim/mappers/PatientMapper.java
+++ lehaim/src/main/java/ru/vaganov/lehaim/mappers/PatientMapper.java
@@ -1,10 +1,9 @@
 package ru.vaganov.lehaim.mappers;
 
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.vaganov.lehaim.models.Patient;
 import ru.vaganov.lehaim.dto.PatientDTO;
+import ru.vaganov.lehaim.models.Patient;
 import ru.vaganov.lehaim.services.DiagnosisCatalogService;
 
 import java.time.LocalDate;
@@ -25,20 +24,19 @@ public abstract class PatientMapper {
     public abstract List<PatientDTO> toDto(List<Patient> entity);
 
 
-
     @BeforeMapping
     protected void beforeMapping(PatientDTO dto, @MappingTarget Patient entity) {
-        if(!dto.getDeathdate().isEmpty()){
+        if (dto.getDeathdate() != null && !dto.getDeathdate().isEmpty()) {
             entity.setDeathdate(LocalDate.parse(dto.getDeathdate(), DateTimeFormatter.ISO_LOCAL_DATE));
         }
-        if(!dto.getBirthdate().isEmpty()){
+        if (dto.getBirthdate() != null && !dto.getBirthdate().isEmpty()) {
             entity.setBirthdate(LocalDate.parse(dto.getBirthdate(), DateTimeFormatter.ISO_LOCAL_DATE));
         }
-        if(!dto.getOperationDate().isEmpty()){
+        if (dto.getOperationDate() != null && !dto.getOperationDate().isEmpty()) {
             entity.setOperationDate(LocalDate.parse(dto.getOperationDate(), DateTimeFormatter.ISO_LOCAL_DATE));
         }
 
-        if(dto.getDiagnosisId() != null){
+        if (dto.getDiagnosisId() != null) {
             entity.setDiagnosis(diagnosisService.findById(dto.getDiagnosisId()));
         }
     }
